4608411a415009f4fc7f21df677c2d726788e649
SoBadCodder <deni-sim@mail.ru>
2022-03-22 12:42:47 +0300

    fix

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,14 +1,17 @@
 const menuLinks = document.querySelectorAll('.menu_main');
 menuLinks.forEach(function (elem) {
     elem.onclick = function (event) {
-        let neighbor = event.target.nextElementSibling;
-        if (neighbor.classList.contains('menu_active')) {
-            neighbor.classList.remove('menu_active');
-            return false;
-        } else if (!neighbor.classList.contains('menu_active')) {
-            event.currentTarget.querySelectorAll('.menu_active').forEach(elem => elem.classList.remove('menu_active'))
-            neighbor.classList.add('menu_active')
-            return false;
+        if (event.target.nextElementSibling != null) {
+            let neighbor = event.target.nextElementSibling;
+            if (neighbor.classList.contains('menu_active')) {
+                neighbor.classList.remove('menu_active');
+                return false;
+            } else if (!neighbor.classList.contains('menu_active')) {
+                event.currentTarget.querySelectorAll('.menu_active').forEach(elem => elem.classList.remove('menu_active'))
+                neighbor.classList.add('menu_active')
+                return false;
+            }
         }
+
     }
 })
